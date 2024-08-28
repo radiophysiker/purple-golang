e549e0743d513b56d79fc5dcb957b029dcc62e01
radiophysiker <bolshakovdmitry@bk.ru>
2024-08-28 11:35:24 +0300

    add scan

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -7,4 +7,13 @@ func main() {
 	const USDtoRUB = 91.13
 	const EURtoRUB = USDtoRUB / USDtoEUR
 	fmt.Printf("Курс евро к рублю: %.2f", EURtoRUB)
+	userInput := scanUserInput("Введите текст: ")
+	fmt.Println("Вы ввели:", userInput)
+}
+
+func scanUserInput(prompt string) string {
+	var input string
+	fmt.Print("Введите число: ")
+	fmt.Scan(&input)
+	return input
 }
